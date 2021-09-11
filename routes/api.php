eea8a8c9fe7941ec5ed28ad5d5015274c19c0b69
Geison Batista dos Santos <geison.santos@cregulacao.ba>
2021-09-11 09:05:52 -0300

    secundary commit

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiDash\HomeController;
+use App\Http\Controllers\IniController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});*/
+
+/*Route::get('/', function(){
+    return response()->json(['status' => true]);
+});*/
+
+//Route::get('/', 'ApiDash\HomeController@index');
+Route::get('/', [HomeController::class, 'index']);
+//Route::get('/', 'IniController@index');
+//Route::get('/', [IniController::class, 'index']);
